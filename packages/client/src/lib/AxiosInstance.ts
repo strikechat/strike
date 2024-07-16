6ee5cfbc28438c0546eb941ae63323fdfafbf0a7
Kamil Grabara <mafineeekdev@gmail.com>
2024-07-16 20:07:13 +0200

    feat: interceptor

--- packages/client/src/lib/AxiosInstance.ts
+++ packages/client/src/lib/AxiosInstance.ts
@@ -6,3 +6,17 @@ export const AxiosInstance = axios.create({
         Authorization: `Bearer ${localStorage.getItem('token')}` || '',
     },
 });
+
+AxiosInstance.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        if (error.response && error.response.status === 403) {
+            window.location.href = '/app';
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default AxiosInstance;
